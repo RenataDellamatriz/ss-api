185c3737e81381dc44c2714be861566e5447f744
Renata Dellamatriz <r.dellamatriz@hotmail.com>
2022-08-17 21:27:21 -0300

    React Front Page

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,53 @@
-import '../styles/App.css';
-import Button from '@mui/material/Button'
-import {useState} from 'react'
-//imports
+import "../styles/App.css";
+import logo from "./spotify-imagem.png";
+import Button from "@mui/material/Button";
+import { borderLeft, shadows } from "@mui/system";
+
+//import {useState} from 'react'
+//fazer imports
 //setar constantes
 
 function App() {
   //useState hook
-  const [score,setScore] = useState(0)
+  //const [score,setScore] = useState(0)
   //declarar e trabalhar com variáveis como js normalmente
+  //procurar sobre desestructuring
 
-  function increment (){
-    setScore(score + 1)
-  }
+  function increment() {}
   return (
-   //código HTML que será exportado do componente
-   <Button onClick={increment}>{score}</Button>
+    //código HTML que será exportado do componente
+    
+    <body>
+      <header>
+        <h1 className="header-text">Spotify Game</h1>
+        </header>      
+      <div className="main">
+      <img src={logo}></img>
+      <Button
+        variant="contained"
+        size="large"
+        sx={{
+          color: "#021403",
+          fontFamily: "Gothic A1",
+          fontSize: '20px',
+          fontStyle: "normal",
+          boxShadow:'3px 4px 3px 3px #17371A',
+          backgroundColor: "#27AE30",
+          height:'50px',
+          width:'200px',          
+          "&:hover": { backgroundColor: "#1F8E26",
+          boxShadow: '3px 4px 3px 3px #17371A' 
+        },
+        
+        }}
+      >
+        Let's play
+      </Button>
+      </div>
+      <footer>
+      <p className="footer-text">&copy;Renatildes</p>
+      </footer>
+    </body>
   );
 }
 
