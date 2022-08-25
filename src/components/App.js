ee5ef2c88350c61f37a5921f4d312e2881000734
Renata Dellamatriz <r.dellamatriz@hotmail.com>
2022-08-25 16:19:22 -0300

    [NEW] Front Page

--- src/components/App.js
+++ src/components/App.js
@@ -1,53 +1,103 @@
-import "../styles/App.css";
-import logo from "./spotify-imagem.png";
-import Button from "@mui/material/Button";
-import { borderLeft, shadows } from "@mui/system";
-
-//import {useState} from 'react'
-//fazer imports
-//setar constantes
+import { Button, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import background from "../media/background-spotify-spotter.png";
+import logo from "../media/Logo-Spotify.png"
 
 function App() {
-  //useState hook
-  //const [score,setScore] = useState(0)
-  //declarar e trabalhar com variáveis como js normalmente
-  //procurar sobre desestructuring
-
-  function increment() {}
   return (
-    //código HTML que será exportado do componente
-    
-    <body>
-      <header>
-        <h1 className="header-text">Spotify Game</h1>
-        </header>      
-      <div className="main">
-      <img src={logo}></img>
-      <Button
-        variant="contained"
-        size="large"
+    <Box
+      sx={{
+        backgroundImage: `url(${background})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        height: "100vh",
+        width: "100%",
+        display:'flex'
+      }}
+    >
+      <Box
         sx={{
-          color: "#021403",
-          fontFamily: "Gothic A1",
-          fontSize: '20px',
-          fontStyle: "normal",
-          boxShadow:'3px 4px 3px 3px #17371A',
-          backgroundColor: "#27AE30",
-          height:'50px',
-          width:'200px',          
-          "&:hover": { backgroundColor: "#1F8E26",
-          boxShadow: '3px 4px 3px 3px #17371A' 
-        },
-        
+          display: "flex",
+          flexDirection: "column",
         }}
       >
-        Let's play
-      </Button>
-      </div>
-      <footer>
-      <p className="footer-text">&copy;Renatildes</p>
-      </footer>
-    </body>
+        <Typography
+          variant="h1"
+          sx={{
+            color: "#00F388",
+            fontSize: "35px",
+            border: "2px solid",
+            borderRadius: "40px",
+            width: "400px",
+            textAlign: "center",
+            padding: "19px",
+            margin: "60px",
+            "@media (max-width: 500px)": {
+              color: "red",
+            },
+          }}
+        >
+          SPOTIFY <strong>SPOTTER</strong>
+        </Typography>
+
+        <Typography
+          sx={{
+            color: "#FCFBFC",
+            fontSize: "60px",
+            marginTop:'90px',
+            marginLeft:'60px'
+          }}
+        >
+          HOW WELL DO YOU
+          <Typography
+            component={"span"}
+            sx={{
+              display: "block",
+              color: "#FCFBFC",
+              fontSize: "60px",
+            }}
+          >
+            KNOW YOUR MUSIC?
+          </Typography>
+        </Typography>
+        <Typography 
+        sx={{
+          color:'#494466',
+          fontSize:'30px',
+          marginTop:'20px',
+          marginLeft:' 60px'
+          }}>TEST YOUR KNOWLEDGE
+          </Typography>
+          <Button  
+          variant="outlined" 
+          sx={{
+            color:'#00F388',
+            fontSize:'30px',
+            border:'2px solid',
+            borderRadius:'15px',
+            width:'250px',
+            height:'70px',
+            marginTop:'30px',
+            marginLeft:'60px',
+            '&:hover':{
+              backgroundColor:'#00F388',
+              color:'#070125',
+              borderColor:'#00F388'
+            }
+          }}>
+            PLAY
+          </Button>
+      </Box>
+      <Box 
+      component="img" 
+      src={logo}
+      sx={{
+        marginTop:'20px',
+        marginLeft:'40px'
+      }}>
+
+      </Box>
+    </Box>
   );
 }
 
